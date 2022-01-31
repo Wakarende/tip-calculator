d6fcca50989a83c1e9483e2549f7ff327937d0ad
Joy Kirii <joykirii@gmail.com>
2022-01-31 22:08:06 +0300

    write logic to get value from bill and people input

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 'use strict';
 
-let totalBill = document.querySelector("#total-bill");
-let tips = document.querySelector('.tips');
+let totalBill = document.getElementById("total-bill");
 
+let tips = document.querySelector('.tips');
+let people = document.getElementById("number-of-people");
+
+
+// Tips button event listener 
 tips.addEventListener('click', e =>{
   if(e.target.matches('button')){
     console.log('tips clicked.');
+    console.log(tips.value);
   }
-})
+});
+
+let bill = totalBill.valueAsNumber;
+console.log(bill);
+
+let numberOfPeople = people.valueAsNumber;
+console.log(numberOfPeople);
+
+// TOTAL TIP CALCULATION 
+function calculateTip(bill, tips, people){
+  let totalTip = (bill * (tips/100))/people;
+  console.log(totalTip);
+  return totalTip;
+}
+
+
+//tip per person
+function tipPerPerson(totalTip, people){
+  let perPersonTip = totalTip / people;
+  console.log(perPersonTip);
+  return perPersonTip;
+
+}
+
