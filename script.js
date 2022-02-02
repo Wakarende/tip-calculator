9a791cd95ed61a43079c823f0aaaed53fb649d98
Joy Kirii <joykirii@gmail.com>
2022-02-02 21:21:37 +0300

    add reset button logic

--- script.js
+++ script.js
@@ -14,20 +14,29 @@ let people = document.getElementById("people");
 people.addEventListener('change', calculateTip);
 
 
+let resetBtn = document.getElementById("reset").addEventListener('click', function reset(){
+  console.log("reset btn clicked!");
+  totalBill.value = 0;
+  people.value = 0;
+  document.getElementById("tip-per-person").textContent = '$0.00';
+  document.getElementById("amout-per-person").textContent = '$0.00';
+
+});
+
 // Get value of totalBill
 
-function bill(ele){
-  if(event.key === 'Enter'){
-    return parseFloat(ele.value);
-  }
-}
+// function bill(ele){
+//   if(event.key === 'Enter'){
+//     return parseFloat(ele.value);
+//   }
+// }
 
 // Get value of people
-function totalPeople(ele){
-  if(event.key === 'Enter'){
-    return parseInt(ele.value);
-  }
-}
+// function totalPeople(ele){
+//   if(event.key === 'Enter'){
+//     return parseInt(ele.value);
+//   }
+// }
 
 // TOTAL TIP CALCULATION 
 function calculateTip(){
@@ -46,13 +55,16 @@ function calculateTip(){
   document.getElementById("tip-per-person").textContent = `\$ ${tipPerPerson}`;
 }
 
+calculateTip();
+
+
 //tip per person 
 
-function tipPerPerson(totalTip, people){
-  let perPersonTip = totalTip / people;
-  console.log(perPersonTip);
-  return perPersonTip;
+// function tipPerPerson(totalTip, people){
+//   let perPersonTip = totalTip / people;
+//   console.log(perPersonTip);
+//   return perPersonTip;
 
-}
+// }
 
 
